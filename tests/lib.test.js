a6da6802ad9d71ee2cd196970150d896494f89e7
Prajwal Pokhrel <prajwal.pokhrel18@gmail.com>
2021-12-22 15:32:58 +0545

    Unit Test - Refactored with Jest Mock Functions (notifyCustomers)

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -96,17 +96,31 @@ describe('applyDiscount', () => {
 
 describe('notifyCustomer', () => {
     it('should send an email to the customer', () => {
-        db.getCustomerSync = function(customerId) {
-            return { email: 'a' };
-        };
+        
+        // const mockFunction = jest.fn();
+        // mockFunction.mockReturnValue(1);
+        // mockFunction.mockResolvedValue(1);
+        // mockFunction.mockRejectedValue(new Error('Some error...'));
+        // const result = await mockFunction();
+        
+        db.getCustomerSync = jest.fn().mockReturnValue({ email: 'a' });
+        // db.getCustomerSync = function(customerId) {
+        //     return { email: 'a' };
+        // };
 
-        let mailSent = false;
-        mail.send = function(email, message) {
-            mailSent = true;
-        };
+        mail.send = jest.fn();
+
+        // let mailSent = false;
+        // mail.send = function(email, message) {
+            // mailSent = true;
+        // };
 
         lib.notifyCustomer({ customerId: 1 });
-        expect(mailSent).toBe(true);
+        // expect(mailSent).toBe(true);
+        expect(mail.send).toHaveBeenCalled();
+        // expect(mail.send).toHaveBeenCalledWith(...);
+        expect(mail.send.mock.calls[0][0]).toBe('a');
+        expect(mail.send.mock.calls[0][1]).toMatch(/order/);
     });
 });
 
