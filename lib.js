3ff3c29e2b2b07994452e0a8a231ae6b7ea279d2
Prajwal Pokhrel <prajwal.pokhrel18@gmail.com>
2021-12-22 14:13:05 +0545

    Refactored absolute function

--- lib.js
+++ lib.js
@@ -1,9 +1,16 @@
 // Testing numbers
+
+// refactored code - tests works just fine
 module.exports.absolute = function(number) {
-    if (number > 0) return number;
-    if (number < 0) return -number;
-    return 0;
+    return (number >= 0) ? number : -number;
 }
+
+// generic code
+// module.exports.absolute = function(number) {
+//     if (number > 0) return number;
+//     if (number < 0) return -number;
+//     return 0;
+// }
 
 // Testing strings
 module.exports.greet = function(name) {
