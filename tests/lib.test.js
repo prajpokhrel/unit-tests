7af829a28e924b54abc4cad3c427dc7597577f44
Prajwal Pokhrel <prajwal.pokhrel18@gmail.com>
2021-12-22 15:21:29 +0545

    Unit Test - Interaction testing (Mock function)

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require('../lib');
 const db = require('../db');
+const mail = require('../mail');
 
 // grouping tests
 describe('absolute', () => {
@@ -91,7 +92,24 @@ describe('applyDiscount', () => {
         lib.applyDiscount(order);
         expect(order.totalPrice).toBe(9);
     });
-})
+});
+
+describe('notifyCustomer', () => {
+    it('should send an email to the customer', () => {
+        db.getCustomerSync = function(customerId) {
+            return { email: 'a' };
+        };
+
+        let mailSent = false;
+        mail.send = function(email, message) {
+            mailSent = true;
+        };
+
+        lib.notifyCustomer({ customerId: 1 });
+        expect(mailSent).toBe(true);
+    });
+});
+
 
 // without grouping
 // test('absolute - should return a positive number if input is positive', () => {
